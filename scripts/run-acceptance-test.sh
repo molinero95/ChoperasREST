f2401b19a91c5aafb392aaaabe5074a16d1d5bab
molinero95 <jaime.molinero.lacave@gmail.com>
2022-03-07 17:18:38 +0100

    Add unit test script an include it to build. Install and remove node_modules in acceptance tests script

--- scripts/run-acceptance-test.sh
+++ scripts/run-acceptance-test.sh
@@ -7,7 +7,10 @@ source "$CURRENT_DIR/start-acceptance-test-environment.sh"
 #TODO: Add healthChecks here 
 
 pushd  "$CURRENT_DIR/../acceptance-test"
+    npm install --quiet
     npm run test
+
+    rm -r node_modules
 popd
 source "$CURRENT_DIR/stop-acceptance-test-environment.sh"
 
