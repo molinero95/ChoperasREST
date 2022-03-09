a5f610a16cf8a66c8379c58f0980914889a3cb41
molinero95 <jaime.molinero.lacave@gmail.com>
2022-03-09 14:23:21 +0100

    Remove configuration log

--- choperas-rest/src/loadConfiguration.js
+++ choperas-rest/src/loadConfiguration.js
@@ -5,6 +5,5 @@ import Configuration from "./configuration.js";
 export function loadConfigurationFromFile(){
     const configuration = fs.readFileSync("./config/config.json");
     Configuration.setConfiguration(JSON.parse(configuration));
-    console.log(Configuration.getConfiguration());
 }
 
